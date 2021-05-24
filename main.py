04910a0ef24ddd4311bfb6d214b78a64b802ce0c
isfopo <55164207+isfopo@users.noreply.github.com>
2021-05-24 09:55:19 -0400

    import iothreads

--- main.py
+++ main.py
@@ -1,48 +1,6 @@
-import time
-import threading
 import mido
-
-class InputThread(threading.Thread):
-  def __init__(self, port):
-    threading.Thread.__init__(self)
-    self.port = port
-    self.ticks = 0
-    self.eighth_note = 0
-  
-  def run(self):
-    with mido.open_input(self.port) as input:
-      for message in map(self.parse_message, input):
-        if message["type"] == "clock":
-          self.ticks += 1
-        elif message["type"] == "songpos":
-          self.ticks = int(message["pos"])
-        else: 
-          print(message)
-
-  def parse_message(self, message):
-    data = str(message).split(" ")
-    output = {}
-    for i, x in enumerate(data):
-      if i == 0: output["type"] = x
-      else: output[x.split("=")[0]] = x.split("=")[1]
-    return output
-
-
-class OutputThread(threading.Thread):
-  def __init__(self, port):
-    threading.Thread.__init__(self)
-    self.port = port
-    self.messages = []
-
-  def run(self):
-    output = mido.open_output(self.port)
-    while True:
-      for message in self.messages:
-        output.send(message)
-        self.messages.remove(message)
-
-  def send(self, message):
-    self.messages.append(message)
+from iostreams import InputThread
+from iostreams import OutputThread
 
 ins = mido.get_input_names()
 input = InputThread(ins[0])
@@ -51,8 +9,3 @@ input.start()
 outs = mido.get_output_names()
 output = OutputThread(outs[0])
 output.start()
-
-ticks = 0
-eighth_note = 0
-
-old_message = None
